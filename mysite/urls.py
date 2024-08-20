9795c78a7c1a3d1a9e05c0ec77200bc48f696568
chava93 <salvador.cmenendez@gmail.com>
2024-08-20 15:31:10 +0000

    Add home page

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ import os
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
+from django.views.generic.base import TemplateView
 
 # Up two folders to serve "site" content
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,6 +29,6 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     re_path(r'^site/(?P<path>.*)$', serve,
         {'document_root': SITE_ROOT, 'show_indexes': True},
-        name='site_path'
-    ),
+        name='site_path'),
+    path('', TemplateView.as_view(template_name='home/main.html'), name="home")
 ]

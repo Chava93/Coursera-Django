ff92166d8f81d3692067790ba81f0293e4183019
chava93 <salvador.cmenendez@gmail.com>
2024-09-02 16:26:05 +0000

    Configure MySQL into the app

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,10 +85,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'salvador93$default',
+        'USER': 'salvador93',
+        'PASSWORD': 'nP8k4xzcTSSH$AA',
+        'HOST': 'salvador93.mysql.pythonanywhere-services.com',
+         'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
